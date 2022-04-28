4d9d3f15002997517ff630a313d300117e441605
Kartik <kartikmalik019@gmail.com>
2022-04-28 20:04:24 +0530

    v1 subquery

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,81 +1,93 @@
-import { Stake } from "../types";
-import {
-  TerraEvent,
-  TerraBlock,
-  TerraMessage,
-  TerraTransaction,
-} from "@subql/types-terra";
-import { MsgExecuteContract } from "@terra-money/terra.js";
-
-// interface Stake {
-//   stake_on_a_club: {
-//     staker: string,
-//     among: string,
-//     club_name: string,
-//   }
-// }
-
-// export async function handleBlock(block: TerraBlock): Promise<void> {
-//   const record = new Block(block.block.block_id.hash);
-//   record.height = BigInt(block.block.block.header.height);
-//   await record.save();
-// }
-
-// export async function handleTransaction(tx: TerraTransaction): Promise<void> {
-//   const record = new Transaction(tx.tx.txhash);
-//   record.blockHeight = BigInt(tx.block.block.block.header.height);
-//   record.timestamp = tx.tx.timestamp;
-//   await record.save();
-// }
+import { Stake, Club } from '../types';
+import { TerraMessage } from '@subql/types-terra';
+import { MsgExecuteContract } from '@terra-money/terra.js';
 
 export async function handleMessage(
   msg: TerraMessage<MsgExecuteContract>
 ): Promise<void> {
+  if(msg.tx.tx.code || msg.tx.tx.codespace) return;
+  if(!Array.isArray(JSON.parse(msg.tx.tx.raw_log))) return;
   logger.info(JSON.stringify(msg.msg.toData().execute_msg));
   const data: any = msg.msg.toData().execute_msg;
   const key = Object.keys(data)[0];
-  if (key === "stake_on_a_club") {
-    const id = `${data.stake_on_a_club.staker}-${data.stake_on_a_club.club_name}`;
-    let record = await Stake.get(id);
-    if (!record) {
-      record = new Stake(id);
-      record.club = data.stake_on_a_club.club_name;
-      record.spender = data.stake_on_a_club.staker;
-      record.stake = BigInt(0);
-    }
-    record.stake += BigInt(data.stake_on_a_club.amount);
-    await record.save();
+  let clubName: string;
+  let sport: string;
+  let clubId: string;
+  let stakeId: string;
+  let stake: Stake;
+  let club: Club;
+  switch (key) {
+    case 'assign_a_club':
+      clubName = data?.assign_a_club?.club_name.split(' - ')?.[0];
+      sport = data?.assign_a_club?.club_name.split(' - ')?.[1] || 'Football';
+      clubId = `${clubName} - ${sport}`;
+      const owner = data?.assign_a_club?.buyer;
+      club = await Club.get(clubId);
+      if (!club) {
+        club = new Club(clubId);
+        club.owner = owner;
+        club.sport = sport;
+        club.club_name = clubId;
+      }
+      await club.save();
+      break;
+    case 'stake_on_a_club':
+      clubId = `${data.stake_on_a_club.club_name} - Football`;
+      stakeId = `${data.stake_on_a_club.staker}-${clubId}`;
+      await addStake(stakeId, {
+        clubId,
+        stakerAddress: data.stake_on_a_club.staker,
+        amount: data.stake_on_a_club.amount,
+      });
+      break;
+    case 'stake_withdraw_from_a_club':
+      if (data.stake_withdraw_from_a_club.immediate_withdrawal === false) {
+        stakeId = `${data.stake_withdraw_from_a_club.staker}-${data.stake_withdraw_from_a_club.club_name} - Football`;
+        stake = await Stake.get(stakeId);
+        if (stake) {
+          stake.amount -= BigInt(data.stake_withdraw_from_a_club.amount);
+          await stake.save();
+        }
+      }
+      break;
+    case 'release_club':
+      clubName = data?.assign_a_club?.club_name.split(' - ')?.[0];
+      sport = data?.assign_a_club?.club_name.split(' - ')?.[1] || 'Football';
+      clubId = `${clubName} - ${sport}`;
+      club = await Club.get(clubId);
+      if (club) {
+        club.owner = null;
+      }
+      await club.save();
+      break;
+    case 'assign_stakes_to_a_club':
+      clubName = data?.assign_stakes_to_a_club?.club_name.split(' - ')?.[0];
+      sport =
+        data?.assign_stakes_to_a_club?.club_name.split(' - ')?.[1] ||
+        'Football';
+      clubId = `${clubName} - ${sport}`;
+      const stakeList = data?.assign_stakes_to_a_club?.stake_list;
+      stakeList.forEach(async (s) => {
+        stakeId = `${s.staker_address}-${clubId}`;
+        await addStake(stakeId, {
+          clubId,
+          stakerAddress: s.staker_address,
+          amount: s.staked_amount,
+        });
+      });
+      break;
+    default:
   }
-  // const record = new Message(`${msg.tx.tx.txhash}-${msg.idx}`);
-  // record.blockHeight = BigInt(msg.block.block.block.header.height);
-  // record.txHash = msg.tx.tx.txhash;
-  // record.contract = msg.msg.toData().contract;
-  // record.sender = msg.msg.toData().sender;
-  // record.executeMsg = JSON.stringify(msg.msg.toData().execute_msg);
-  // await record.save();
 }
 
-// export async function handleEvent(
-//   event: TerraEvent<MsgExecuteContract>
-// ): Promise<void> {
-//   const record = new TransferEvent(
-//     `${event.tx.tx.txhash}-${event.msg.idx}-${event.idx}`
-//   );
-//   record.blockHeight = BigInt(event.block.block.block.header.height);
-//   record.txHash = event.tx.tx.txhash;
-//   for (const attr of event.event.attributes) {
-//     switch (attr.key) {
-//       case "sender":
-//         record.sender = attr.value;
-//         break;
-//       case "recipient":
-//         record.recipient = attr.value;
-//         break;
-//       case "amount":
-//         record.amount = attr.value;
-//         break;
-//       default:
-//     }
-//   }
-//   await record.save();
-// }
+const addStake = async (stakeId, { clubId, stakerAddress, amount }) => {
+  let stake = await Stake.get(stakeId);
+  if (!stake) {
+    stake = new Stake(stakeId);
+    stake.clubId = clubId;
+    stake.staker = stakerAddress;
+    stake.amount = BigInt(0);
+  }
+  stake.amount += BigInt(amount);
+  await stake.save();
+};
